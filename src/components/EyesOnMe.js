bafa9337e5bc5106c6a94c8f3ab1bbd1ca83e739
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-02 15:22:09 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -3,12 +3,12 @@ import React from 'react'
 
 export default class EyesOnMe extends React.Component {
 
-  blurHandler = () => { console.log('Hey! Eyes on me!')}
+  handleBlur = () => { console.log('Hey! Eyes on me!')}
   focusHandler = () => { console.log('Good!')}
   
   render() {
     return(
-      <button onBlur={this.blurHandler} onFocus={this.focusHandler}>Press Me</button>
+      <button onBlur={this.handleBlur} onFocus={this.focusHandler}>Press Me</button>
     )
   }
 }
