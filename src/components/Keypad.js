ac408764898cf0161045e241b84ef6144e274010
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-02 15:20:55 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,11 +3,11 @@ import React from 'react'
 
 export default class Keypad extends React.Component {
 
-  keyHandler = () => { console.log('Entering password...')}
+  handleKey = () => { console.log('Entering password...')}
   
   render() {
     return(
-      <input type='password' onKeyUp={this.keyHandler}/>
+      <input type='password' onKeyUp={this.handleKey}/>
     )
   }
 }
